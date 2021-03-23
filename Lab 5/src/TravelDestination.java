fbe107d0b9f2c7fadc3764a9f4bdab480160411e
jadendap <80736924+jadendap@users.noreply.github.com>
2021-03-23 14:46:03 -0500

    Update TravelDestination.java
    Added a print statement.

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -28,6 +28,7 @@ public class TravelDestination {
 		System.out.println("My name is Duy Le. I wish to travel to Japan once this Covid goes away!");
 		System.out.println("My name is Jeffrey Flowers. I wish to travel to California once this Covid goes away!");
 		System.out.println("My name is Minh Le. I wish to travel to Japan once this Covid goes away!");
+		System.out.println("My name is Jaden Dapilmoto. I wish to travel to Hawaii once this Covid goes away!");
 	}
 
 }
